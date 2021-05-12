de3275095dc7c985e03574053b60a8f6eb7dab63
szymon27 <42315411+szymon27@users.noreply.github.com>
2021-05-12 15:25:50 +0200

    car class

---
+++ Karrent/Objects/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karrent.Objects
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public CarDetails CarDetails { get; set; }
+        public string PlateNumber { get; set; }
+        public double Mileage { get; set; }
+        public bool IsActive{ get; set; }
+        public DateTime InspectionDate { get; set; }
+
+        public Car(int id, CarDetails carDetails, string plateNumber, double mileage, bool isActive, DateTime inspectionDate)
+        {
+            this.Id = id;
+            this.CarDetails = carDetails;
+            this.PlateNumber = plateNumber;
+            this.Mileage = mileage;
+            this.IsActive = isActive;
+            this.InspectionDate = inspectionDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Id:{this.Id} " +
+                $"Car details:{this.CarDetails.ToString()} " +
+                $"Plate number:{this.PlateNumber} " +
+                $"Mileage:{this.Mileage} " +
+                $"Is active:{this.IsActive} " +
+                $"Inspection date:{this.InspectionDate:dd-MM-yyyy}";
+        }
+    }
+}
