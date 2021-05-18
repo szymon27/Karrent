0cc56011551b9f4a209726e9559f582e42ef40ea
makskaf <m.kafka00@gmail.com>
2021-05-18 14:20:58 +0200

    Control panel views

---
+++ Karrent/Views/ControlPanelWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Karrent.Views
+{
+    /// <summary>
+    /// Interaction logic for ControlPanelWindow.xaml
+    /// </summary>
+    public partial class ControlPanelWindow : Window
+    {
+        public ControlPanelWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddCarDone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnImg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditUserDone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDesactivateUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbCarDetails_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
